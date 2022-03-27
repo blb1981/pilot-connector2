474f3cafe4ee42401ab8653ec3b76ddb30002918
Brandon <brandonbland90@gmail.com>
2022-03-27 10:01:11 -0400

    Added model factory methods to user controller

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,101 +1,90 @@
-const User = require('../models/User')
+const { Op } = require('sequelize')
 
+const User = require('../models/User')
+const modelFactory = require('../utils/modelFactory')
 const apiResponse = require('../utils/apiResponse')
+const errorResponse = require('../utils/errorResponse')
 
 // Hard delete
 exports.deleteHard = async (req, res) => {
-  try {
-    const user = await User.findByPk(req.params.id, { paranoid: false })
-
-    if (!user) {
-      return apiResponse(res, 400, 'User not found')
-    }
-
-    await user.destroy({ force: true })
-    apiResponse(res, 200, 'User hard deleted', null)
-  } catch (error) {
-    apiResponse(res, 500, 'Server error', null)
-  }
+  await modelFactory.deleteHard(User, req, res, 'User')
 }
 
 // Get all including soft delete
 exports.getAllSoft = async (req, res) => {
-  try {
-    const users = await User.findAll({ paranoid: false })
-    apiResponse(res, 200, 'List of all users, including soft deleted', users)
-  } catch (error) {
-    apiResponse(res, 500, 'Server error', null)
-  }
+  await modelFactory.getAllSoft(User, req, res, 'User')
 }
 
 // Get all
+/*
+    Documentation on search queries
+    Allowed parameters - orderBy, order, offset, limit, search
+*/
+
 exports.getAll = async (req, res) => {
-  const { orderBy, order, limit, offset } = req.query
-  console.log(req.query)
-  // TODO: Finish query parameters filtering
+  // Destructure query parameters, set default values
+  const {
+    orderBy = 'id',
+    order = 'ASC',
+    offset = 0,
+    limit,
+    search = '',
+  } = req.query
 
   try {
-    const users = await User.findAll()
-    apiResponse(res, 200, 'List of all users', users)
+    // Find all, allow text search through headlines and descriptions
+    const users = await User.findAll({
+      attributes: { exclude: ['password'] },
+      where: {
+        [Op.or]: {
+          companyContact: { [Op.substring]: search },
+          companyName: { [Op.substring]: search },
+          phone: { [Op.substring]: search },
+          email: { [Op.substring]: search },
+        },
+      },
+      order: [[orderBy, order]],
+      limit,
+      offset,
+    })
+
+    apiResponse(res, 200, 'List of all users', {
+      count: users.length,
+      results: users,
+    })
   } catch (error) {
-    console.log(error)
-    apiResponse(res, 500, 'Server error', error)
+    errorResponse(res, error)
   }
 }
 
 // Create
 exports.createOne = async (req, res) => {
-  try {
-    const user = await User.create(req.body)
-    apiResponse(res, 201, 'User created', [user])
-  } catch (error) {
-    apiResponse(res, 500, 'Server error', null)
-  }
+  await modelFactory.createOne(User, req, res, 'User')
 }
 
 // Get single
 exports.getOne = async (req, res) => {
   try {
-    const user = await User.findByPk(req.params.id)
+    const result = await User.findByPk(req.params.id, {
+      attributes: { exclude: ['password'] },
+    })
 
-    if (!user) {
-      return apiResponse(res, 400, 'User not found')
+    if (!result) {
+      return apiResponse(res, 400, `User not found`)
     }
 
-    apiResponse(res, 200, 'Single user', [user])
+    apiResponse(res, 200, `Single User`, [result])
   } catch (error) {
-    apiResponse(res, 500, 'Server error', null)
+    errorResponse(res, error)
   }
 }
 
 // Update single
 exports.updateOne = async (req, res) => {
-  try {
-    const user = await User.findByPk(req.params.id)
-
-    if (!user) {
-      return apiResponse(res, 400, 'User not found')
-    }
-
-    const updatedUser = await user.update(req.body)
-    apiResponse(res, 200, 'User updated', [updatedUser])
-  } catch (error) {
-    apiResponse(res, 500, 'Server error', null)
-  }
+  await modelFactory.updateOne(User, req, res, 'User')
 }
 
 // Delete single
 exports.deleteOne = async (req, res) => {
-  try {
-    const user = await User.findByPk(req.params.id)
-
-    if (!user) {
-      return apiResponse(res, 200, 'User not found')
-    }
-
-    await user.destroy()
-    apiResponse(res, 200, 'User deleted', null)
-  } catch (error) {
-    apiResponse(res, 500, 'Server error', null)
-  }
+  await modelFactory.deleteOne(User, req, res, 'User')
 }
